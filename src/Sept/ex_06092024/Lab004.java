f280665a72f3a8efec983031a4ff3122dd32a95e
sharbanis-lab <sharbanis@gmail.com>
2024-09-16 18:01:24 +0530

    Add the files

--- src/Sept/ex_06092024/Lab004.java
+++ src/Sept/ex_06092024/Lab004.java
@@ -1,9 +1,14 @@
 package Sept.ex_06092024;
 
+
 public class Lab004 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(2+2);
+
     }
+
+
 
 }
