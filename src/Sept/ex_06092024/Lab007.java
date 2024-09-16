936f51a4c136a2eea2382ca3f895b091a1ccdcb6
sharbanis-lab <sharbanis@gmail.com>
2024-09-16 21:41:38 +0530

    Add the files

---
+++ src/Sept/ex_06092024/Lab007.java
@@ -0,0 +1,13 @@
+package Sept.ex_06092024;
+// Below code is to under single line comment and Multiple line comment
+public class Lab007 {
+    public static void main(String[] Sharbani) {
+     System.out.println ("Hello Sir!");
+//   Single line comment
+
+        /* method to write multipleline cmments
+        * comment 2
+        * comment 3
+        * */
+}
+}
