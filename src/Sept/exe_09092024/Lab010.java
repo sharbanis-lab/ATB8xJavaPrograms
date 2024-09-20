629a2d5503af471124af98c5a594dc907c1d75d7
sharbanis-lab <sharbanis@gmail.com>
2024-09-20 13:15:58 +0530

    Add the files

---
+++ src/Sept/exe_09092024/Lab010.java
@@ -0,0 +1,12 @@
+package Sept.exe_09092024;
+
+public class Lab010 {
+    public static void main(String[] args) {
+        //Keywords //
+        //Reserved words//
+        //Simple words -> predefined in Java
+        //In IntelliJ they are in blue colour//
+        //Package, public class, static void//
+    }
+
+}
---
+++ src/Sept/exe_09092024/Lab011.java
@@ -0,0 +1,9 @@
+package Sept.exe_09092024;
+
+public class Lab011 {
+    public static void main(String[] args) {
+        //Variables & Data Types and Identifiers//
+        boolean is_mohit_has_car = true;
+        boolean do_you_have_audi = false;
+    }
+}
