a13a92c04871371354579cee0b3e8a3b3b298674
sharbanis-lab <sharbanis@gmail.com>
2024-09-20 09:08:49 +0530

    Add the files

--- src/Sept/exe_09092024/Lab009.java
+++ src/Sept/exe_09092024/Lab009.java
@@ -2,7 +2,7 @@ package Sept.exe_09092024;
 
 public class Lab009 {
     public static void main(String[] Sharbani) {
- System.out.println ("Hello World!");
+ System.out.println ("Hello Sharbani!");
  //single line comment//
         /* Multi line
 
